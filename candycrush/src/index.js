fc553b9cbe27812f4cdfb8ed8a2acf49386e5449
Delon509 <cdelon509@gmail.com>
2023-03-07 16:25:49 +0800

    Update Layout

--- candycrush/src/index.js
+++ candycrush/src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 //import './index.css';
-import Crush from './Crush';
+import Crush from "./Crush";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Crush />
   </React.StrictMode>
 );
-
-
